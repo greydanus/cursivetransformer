b8e0cbc446842811a1aa9d79f390d26f9bfb4ecb
Sam <greydanus@users.noreply.github.com>
2024-08-24 10:38:50 -0700

    Small nvidia driver upgrade

--- paperspace.sh
+++ paperspace.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install linux-headers-$(uname -r) build-essential -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y && sudo apt update
-sudo apt install nvidia-driver-525 -y
+sudo apt install nvidia-driver-560 -y
 sudo reboot
 nvidia-smi
