612834b90f1fdddb1aca8536354b9864a7cbca9a
zwimpee <zwimpee@gmail.com>
2024-09-16 13:20:34 -0500

    fix broken code

--- sample.py
+++ sample.py
@@ -176,4 +176,3 @@ if __name__ == '__main__':
     save_samples(model, test_dataset, num=6, do_sample=True, log_wandb=False)
     save_samples(model, test_dataset, num=6, do_sample=False, log_wandb=False)
     sys.exit()
-
