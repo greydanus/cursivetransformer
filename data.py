e412cdd6cb2d46c00f6b1266f80300fe6139572d
zwimpee <zwimpee@gmail.com>
2024-10-27 10:15:55 -0500

    fix indentation

--- data.py
+++ data.py
@@ -236,8 +236,8 @@ class StrokeDataset(Dataset):
         return encoded.flatten()
 
     def decode_stroke(self, ix):
-      ix_list = self.split_by_word_tokens(ix)
-      return [self.decode_word_strokes(ix) for ix in ix_list]
+        ix_list = self.split_by_word_tokens(ix)
+        return [self.decode_word_strokes(ix) for ix in ix_list]
 
     def decode_word_strokes(self, ix):
         if isinstance(ix, torch.Tensor):
@@ -311,38 +311,38 @@ class StrokeDataset(Dataset):
 
 
 def create_datasets(args):
-  np.random.seed(args.seed) ; torch.manual_seed(args.seed)
-  data = load_and_parse_data(args.dataset_name)
+    np.random.seed(args.seed) ; torch.manual_seed(args.seed)
+    data = load_and_parse_data(args.dataset_name)
 
-  # partition the input data into a training and the test set
-  test_set_size = min(1000, max(10, int(len(data) * 0.05))) # between 10 and 1000 examples: ideally 10% of dataset
-  rp = torch.randperm(len(data)).tolist()
+    # partition the input data into a training and the test set
+    test_set_size = min(1000, max(10, int(len(data) * 0.05))) # between 10 and 1000 examples: ideally 10% of dataset
+    rp = torch.randperm(len(data)).tolist()
 
-  train_examples = generate_word_combos([data[i] for i in rp[:-test_set_size]], desired_num_combos=args.train_size, num_words=args.num_words)
-  train_examples = [train_examples[i] for i in torch.randperm(len(train_examples)).tolist()]
+    train_examples = generate_word_combos([data[i] for i in rp[:-test_set_size]], desired_num_combos=args.train_size, num_words=args.num_words)
+    train_examples = [train_examples[i] for i in torch.randperm(len(train_examples)).tolist()]
 
-  test_examples = generate_word_combos([data[i] for i in rp[-test_set_size:]], desired_num_combos=args.test_size, num_words=args.num_words)
-  test_examples = [test_examples[i] for i in torch.randperm(len(test_examples)).tolist()]
+    test_examples = generate_word_combos([data[i] for i in rp[-test_set_size:]], desired_num_combos=args.test_size, num_words=args.num_words)
+    test_examples = [test_examples[i] for i in torch.randperm(len(test_examples)).tolist()]
 
-  train_word_strokes = [[copy.deepcopy(stroke) for stroke in v['points']] for v in train_examples]
-  train_texts = [copy.deepcopy(v['metadata']['asciiSequence']) for v in train_examples]
+    train_word_strokes = [[copy.deepcopy(stroke) for stroke in v['points']] for v in train_examples]
+    train_texts = [copy.deepcopy(v['metadata']['asciiSequence']) for v in train_examples]
 
-  test_word_strokes = [[copy.deepcopy(stroke) for stroke in v['points']] for v in test_examples]
-  test_texts = [copy.deepcopy(v['metadata']['asciiSequence']) for v in test_examples]
+    test_word_strokes = [[copy.deepcopy(stroke) for stroke in v['points']] for v in test_examples]
+    test_texts = [copy.deepcopy(v['metadata']['asciiSequence']) for v in test_examples]
 
-  print(f"Number of examples in the train dataset: {len(train_examples)}")
-  print(f"Number of examples in the test dataset: {len(test_examples)}")
-  print(f"Average number of words per example: {np.mean([len(strokes) for strokes in train_word_strokes]):.1f}")
-  print(f"Max token sequence length: {args.max_seq_length}")
-  print(f"Number of unique characters in the ascii vocabulary: {len(args.alphabet)}")
-  print("Ascii vocabulary:")
-  print(f'\t"{args.alphabet}"')
-  print(f"Split up the dataset into {len(train_examples)} training examples and {len(test_examples)} test examples")
+    print(f"Number of examples in the train dataset: {len(train_examples)}")
+    print(f"Number of examples in the test dataset: {len(test_examples)}")
+    print(f"Average number of words per example: {np.mean([len(strokes) for strokes in train_word_strokes]):.1f}")
+    print(f"Max token sequence length: {args.max_seq_length}")
+    print(f"Number of unique characters in the ascii vocabulary: {len(args.alphabet)}")
+    print("Ascii vocabulary:")
+    print(f'\t"{args.alphabet}"')
+    print(f"Split up the dataset into {len(train_examples)} training examples and {len(test_examples)} test examples")
 
-  # wrap in dataset objects
-  train_dataset = StrokeDataset(train_word_strokes, train_texts, args, name='train')
-  test_dataset = StrokeDataset(test_word_strokes, test_texts, args, name='test')
-  return train_dataset, test_dataset
+    # wrap in dataset objects
+    train_dataset = StrokeDataset(train_word_strokes, train_texts, args, name='train')
+    test_dataset = StrokeDataset(test_word_strokes, test_texts, args, name='test')
+    return train_dataset, test_dataset
 
 
 class InfiniteDataLoader:
