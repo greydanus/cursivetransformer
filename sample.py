46e5ee0b14ce901baa5969943b957e92b2ceb12d
Sam <greydanus@users.noreply.github.com>
2025-03-21 11:07:53 -0700

    Small fix

--- sample.py
+++ sample.py
@@ -154,7 +154,7 @@ def generate_helper_fn(model, dataset, word_list, params):
     
     def trunc_or_pad_words(word_list, dataset, params):
         n = len(word_list) ; n_words = params.n_words
-        if n > n_words:
+        if n >= n_words:
             # if params.verbose: print(f"Expected {n_words} words, got {n}; truncating")
             return word_list[:n_words-1]
         elif n < n_words:  # Sample random words from the dataset's vocabulary
